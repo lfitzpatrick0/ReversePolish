be64204834a2ae938688da8db2461068d3b698dd
User <user@MacBooks-MacBook-Air.local>
2018-03-28 18:45:27 -0700

    Working nested loop.  loop.cpp will become main.cpp

--- src/loop.cpp
+++ src/loop.cpp
@@ -1,28 +1,83 @@
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
 
 //loop test cause my code is garbage.
 int main()
 {
     char answer = 'y';
-    string line = "abcdef";
+    string line;
     int i = 0;
+    stack<int> st;
+
+    cout << "Enter a postfix: ";
+    getline(cin,line);
+
     while(answer == 'y')
     {
         while(line[i])
         {
-            cout << line[i] << " ";
+            if(isalpha(line[i]))
+            {
+                cout << "Enter " << line[i] << ": ";
+                int temp;
+                cin >> temp;
+                st.push(temp);
+            }
+            else
+            {
+                int left, right = 0;
+                switch(line[i])
+                {
+                    case '+':
+                        right = st.top();
+                        st.pop();
+                        left = st.top();
+                        st.pop();
+                        st.push(left+right);
+                        break;
+                    case '-':
+                        right = st.top();
+                        st.pop();
+                        left = st.top();
+                        st.pop();
+                        st.push(left-right);
+                        break;
+                    case '*':
+                        right = st.top();
+                        st.pop();
+                        left = st.top();
+                        st.pop();
+                        st.push(left*right);
+                        break;
+                    case '/':
+                        right = st.top();
+                        st.pop();
+                        left = st.top();
+                        st.pop();
+                        st.push(left/right);
+                        break;                        
+                    default:
+                        break;
+                }
+            }
             i++;
         }
-        cout << "Continue?" << endl;
+        cout << "Solution: " << st.top() << endl;
+        cout << "Continue? (Y/N): " << endl;
+        cin.ignore();
         cin >> answer;
-        if(answer == 'y')
+        if(tolower(answer) == 'y')
         {
             i = 0;
-            cout << "Enter string: ";
+            cout << "Enter a postfix: ";
             cin.ignore();
             getline(cin, line);
+        }
+        else
+        {
+            break;
         }
     }
 
